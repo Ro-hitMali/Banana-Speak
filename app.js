194af9a33611d6fe8c671ec7e44cc449422bc1fd
Ro-hitMali <rm4701740@gmail.com>
2022-12-12 17:44:09 +0530

    used api

--- app.js
+++ app.js
@@ -2,13 +2,33 @@ var buttonTranslate = document.querySelector("#btn-translate");
 var textInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+//var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
 
-console.log(outputDiv);
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
-function clickHandler() {
-    outputDiv.innerText = "jfbgfufgfuf" + textInput.value;
+function getTranslationURL (text){
+    return serverURL + "?" + text
 }
 
+function errorHandler(error){
+    console.log("Error occured", error)
+    alert("something wromg with server. Try again after some time!! Sorry for the inconvenience caused")
+}
+
+function clickHandler() {
+  //  outputDiv.innerText = "jfbgfufgfuf" + textInput.value;
+
+  var inputText = textInput.value;
+
+  //calling server for processing
+  fetch(getTranslationURL(inputText)) 
+  .then(response => response.json())
+  .then(json => {
+    var translateTxt = json.content.translated
+    outputDiv.innerText = translateTxt;
+   })
+    .catch(errorHandler)
+};
 
 buttonTranslate.addEventListener("click", clickHandler);
 
